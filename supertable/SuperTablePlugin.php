9104edb805fb33b912c3a22591698a89cf9c47ea
Josh Crawford <joshua@sgroup.com.au>
2016-08-30 00:11:53 +1000

    version 1.0.1

--- supertable/SuperTablePlugin.php
+++ supertable/SuperTablePlugin.php
@@ -14,7 +14,7 @@ class SuperTablePlugin extends BasePlugin
 
     public function getVersion()
     {
-        return '1.0.0';
+        return '1.0.1';
     }
 
     public function getSchemaVersion()
