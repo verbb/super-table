dbcd4b86d1100d50fafc8ec4edff6f2386172078
Josh Crawford <joshua@sgroup.com.au>
2016-11-22 15:28:00 +1100

    Support for Craft 2.6.2951

--- supertable/SuperTablePlugin.php
+++ supertable/SuperTablePlugin.php
@@ -48,9 +48,16 @@ class SuperTablePlugin extends BasePlugin
     }
 
     public function onBeforeInstall()
-    {   
+    {
+        $version = craft()->getVersion();
+
+        // Craft 2.6.2951 deprecated `craft()->getBuild()`, so get the version number consistently
+        if (version_compare(craft()->getVersion(), '2.6.2951', '<')) {
+            $version = craft()->getVersion() . '.' . craft()->getBuild();
+        }
+
         // Craft 2.3.2615 getFieldsForElementsQuery()
-        if (version_compare(craft()->getVersion() . '.' . craft()->getBuild(), '2.3.2615', '<')) {
+        if (version_compare($version, '2.3.2615', '<')) {
             throw new Exception($this->getName() . ' requires Craft CMS 2.3.2615+ in order to run.');
         }
     }
