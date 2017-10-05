0c81224cc5d253a303f41c29dfb15dedfb3d03e2
Josh Crawford <joshua@sgroup.com.au>
2017-10-05 13:39:09 +1100

    Feed Me - improve Matrix-handling

--- supertable/integrations/feedme/fields/SuperTableFeedMeFieldType.php
+++ supertable/integrations/feedme/fields/SuperTableFeedMeFieldType.php
@@ -30,29 +30,79 @@ class SuperTableFeedMeFieldType extends BaseFeedMeFieldType
 
         // Store the fields for this Matrix - can't use the fields service due to context
         $blockTypes = craft()->superTable->getBlockTypesByFieldId($field->id, 'id');
-        $blockType = reset($blockTypes);
 
-        $prePreppedData = array();
+        // Ensure when importing only one block thats its treated correctly.
+        $keys = array_keys($data);
+
+        if (!is_numeric($keys[0])) {
+            $data = array($data);
+        }
+
+        // If we've got Matrix data like 0.0.blockHandle.fieldHandle - then we've got a problem.
+        // Commonly, this will be due to a field being referenced outside of inner-repeatable Matrix data.
+        $hasOrphanedData = false;
 
         foreach (Hash::flatten($data) as $key => $value) {
             if (preg_match('/^\d+\.\d+/', $key)) {
-                $prePreppedData[$key] = $value;
-            } else {
-                $prePreppedData['0.' . $key] = $value;
+                $hasOrphanedData = true;
+                break;
             }
         }
 
-        $data = Hash::expand($prePreppedData);
+        if ($hasOrphanedData) {
+            $seperateBlockData = array();
+
+            foreach ($data as $sortKey => $sortData) {
+                foreach ($sortData as $blockHandle => $blockFieldData) {
+                    if (!is_numeric($blockHandle)) {
+                        $seperateBlockData[$blockHandle] = $blockFieldData;
+
+                        unset($data[$sortKey][$blockHandle]);
+                    }
+                }
+            }
+
+            // Now, append this content to each block that we're importing, so it gets sorted out properly
+            if ($seperateBlockData) {
+                foreach ($data as $sortKey => $sortData) {
+                    foreach ($sortData as $blockHandle => $blockFieldData) {
+                        $data[$sortKey][$blockHandle] = array_merge_recursive($blockFieldData, $seperateBlockData);
+                    }
+                }
+
+                $data = $data[0];
+            }
+        }
+
+        if (!isset($data[0])) {
+            $data = array($data);
+        }
 
         foreach ($data as $sortKey => $sortData) {
-            $preppedFieldData = array();
+            $blockData = array();
 
             foreach ($sortData as $blockHandle => $blockFieldData) {
+                $preppedFieldData = array();
+
                 foreach ($blockFieldData as $blockFieldHandle => $blockFieldContent) {
+
+                    if (!isset($blockTypes[$blockHandle])) {
+                        continue;
+                    }
+
+                    // Get the Matrix-contexted field for our regular field-prepping function
+                    $blockType = $blockTypes[$blockHandle];
 
                     foreach ($blockType->getFields() as $f) {
                         if ($f->handle == $blockFieldHandle) {
                             $subField = $f;
+                        }
+                    }
+
+                    // Check to see if this is information for a block
+                    if ($blockFieldHandle == 'block') {
+                        foreach ($blockFieldContent as $blockFieldOption => $blockFieldOptionValue) {
+                            $blockData[$blockHandle][$blockFieldOption] = Hash::get($blockFieldOptionValue, 'data');
                         }
                     }
 
@@ -83,26 +133,22 @@ class SuperTableFeedMeFieldType extends BaseFeedMeFieldType
                         $preppedFieldData[$blockFieldHandle] = $parsedData;
                     }
                 }
+
+                if ($preppedFieldData) {
+                    $order = $sortKey + 1;
+                    $enabled = true;
+
+                    $preppedData['new' . $order] = array(
+                        'type' => $blockHandle,
+                        'order' => $order,
+                        'enabled' => $enabled,
+                        'fields' => $preppedFieldData,
+                    );
+                }
             }
-
-            $order = $sortKey + 1;
-
-            $preppedData['new' . $order] = array(
-                'type' => $blockHandle,
-                'order' => $order,
-                'enabled' => true,
-                'fields' => $preppedFieldData,
-            );
         }
 
         return $preppedData;
-    }
-
-    // Allows us to smartly-check to look at existing Matrix fields for an element, and whether data has changed or not.
-    // No need to update Matrix blocks unless content has changed, which causes needless new elements to be created.
-    public function postFieldData($element, $field, &$data, $handle)
-    {
-        
     }
     
 }
