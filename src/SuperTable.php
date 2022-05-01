d41bb2822ea1bd4ac639304abc8f3ac81dd8c482
Josh Crawford <joshua@sgroup.com.au>
2022-05-01 10:38:51 +1000

    Rename base plugin methods

--- src/SuperTable.php
+++ src/SuperTable.php
@@ -50,8 +50,8 @@ class SuperTable extends Plugin
 
         self::$plugin = $this;
 
-        $this->_setPluginComponents();
-        $this->_setLogging();
+        $this->_registerComponents();
+        $this->_registerLogTarget();
         $this->_registerCpRoutes();
         $this->_registerVariables();
         $this->_registerFieldTypes();
--- src/base/PluginTrait.php
+++ src/base/PluginTrait.php
@@ -3,12 +3,11 @@ namespace verbb\supertable\base;
 
 use verbb\supertable\SuperTable;
 use verbb\supertable\services\SuperTableService;
+use verbb\base\BaseHelper;
 
 use Craft;
 
 use yii\log\Logger;
-
-use verbb\base\BaseHelper;
 
 trait PluginTrait
 {
@@ -21,13 +20,17 @@ trait PluginTrait
     // Static Methods
     // =========================================================================
     
-    public static function log($message): void
+    public static function log(string $message, array $params = []): void
     {
+        $message = Craft::t('super-table', $message, $params);
+
         Craft::getLogger()->log($message, Logger::LEVEL_INFO, 'super-table');
     }
 
-    public static function error($message): void
+    public static function error(string $message, array $params = []): void
     {
+        $message = Craft::t('super-table', $message, $params);
+
         Craft::getLogger()->log($message, Logger::LEVEL_ERROR, 'super-table');
     }
 
@@ -44,7 +47,7 @@ trait PluginTrait
     // Private Methods
     // =========================================================================
 
-    private function _setPluginComponents(): void
+    private function _registerComponents(): void
     {
         $this->setComponents([
             'service' => SuperTableService::class,
@@ -53,7 +56,7 @@ trait PluginTrait
         BaseHelper::registerModule();
     }
 
-    private function _setLogging(): void
+    private function _registerLogTarget(): void
     {
         BaseHelper::setFileLogging('super-table');
     }
